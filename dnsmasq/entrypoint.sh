51bacc1f855ae749ff0ee44c05e12f3e4f352ade
JinnLynn <eatfishlin@gmail.com>
2017-12-11 16:01:50 +0800

    optimize

--- dnsmasq/entrypoint.sh
+++ dnsmasq/entrypoint.sh
@@ -7,6 +7,17 @@ if [ "${1:0:1}" = '-' ]; then
     set -- dnsmasq "$@"
 fi
 
+# 允许所有的 OUTPUT 流量 系统默认就是ACCEPT
+iptables -P OUTPUT ACCEPT
+# 允许所有 INPUT 流量 系统默认就是ACCEPT
+iptables -P INPUT ACCEPT
+
+#! 允许转发，实现NAT !重要 系统默认是DROP
+iptables -P FORWARD ACCEPT
+# 临时有效，重启网络或系统都会失效，永久需修改/etc/sysctl.conf
+# echo 1 > /proc/sys/net/ipv4/ip_forward
+sysctl -w net.ipv4.ip_forward=1
+
 # 创建IPSet list
 ipset list $REDIR_SET 1>/dev/null 2>&1 || ipset create $REDIR_SET hash:ip
 
@@ -17,10 +28,5 @@ iptables -t nat -S | grep -q $REDIR_SET && true || {
         iptables -t nat -A $chain -p tcp -m set --match-set $REDIR_SET dst -j REDIRECT --to-port $REDIR_PORT
     done
 }
-
-# 允许转发，实现NAT
-# 临时有效，重启网络或系统都会失效，永久需修改/etc/sysctl.conf
-# echo 1 > /proc/sys/net/ipv4/ip_forward
-sysctl -w net.ipv4.ip_forward=1
 
 exec $@
