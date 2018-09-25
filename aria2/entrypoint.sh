c483d46fb8c96f9118e1ad6a824b1f8c9dc48ee6
JinnLynn <eatfishlin@gmail.com>
2018-09-25 15:00:33 +0800

    remove ARIA2_INPUT

--- aria2/entrypoint.sh
+++ aria2/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
 
-ARIA2_INPUT=${ARIA2_INPUT:-/app/run/aria2.session}
 ARIA2_CONFIG=${AEIA2_CONFIG:-/app/etc/aria2.conf}
 
 # 没有指定命令
@@ -8,10 +7,6 @@ if [ -z "$@" ]; then
     opt=""
     if [ -n "$ARIA2_CONFIG" ]; then
         opt="$opt --conf-path=$ARIA2_CONFIG"
-    fi
-    if [ -n "$ARIA2_INPUT" ]; then
-        touch $ARIA2_INPUT_FILE
-        opt="$opt --input-file=$ARIA2_INPUT"
     fi
     exec aria2c $opt
 fi
