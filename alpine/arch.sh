702c31cc79f18e86fb6c2e1974ebc841cd274bbb
JinnLynn <eatfishlin@gmail.com>
2018-10-10 10:43:42 +0800

    arch.sh update

--- alpine/arch.sh
+++ alpine/arch.sh
@@ -1,114 +1,131 @@
-#!/bin/sh
+#!/usr/bin/env sh
 #
-# CPU架构信息，默认使用alpine的架构描述: x86 x86_64 armhf aarch64
-# 也可使用GO语言的架构描述或自定义各架构描述
-# 
+# CPU架构信息
+# 386   amd64   arm     arm64     默认: GO语言的架构描述
+#                                 原生:
+# x86   x86_64  armhf   aarch64     alpine: 等同于于`apk --print-arch`
+# i386  amd64   armhf   arm64       debian/ubuntu/raspbian: 等同于`dpkg --print-architecture`
+#
+# 也可自定义描述
+#
 # AUTHOR:   JinnLynn <eatfishlin@gmail.com>
-# DATE:     2018-06-13
+# DATE:     2018-09-26
 #
 
-ARCH=$(apk --print-arch)
+VERSION=2018.09.26
+# 架构的系统原生描述
+ARCH=
+# 是否使用当前环境原生输出
+USE_RAW=
+# 要比较的字符串
+CHECK_ARCH=
 
-# 是否使用GO语言的架构描述
-USE_GO_ARCH_TPL=
+which dpkg 1>/dev/null 2>&1 && ARCH=$(dpkg --print-architecture)
+which apk 1>/dev/null 2>&1 && ARCH=$(apk --print-arch)
 
-# 自定义架构描述
-ARCH_X86=
-ARCH_X86_64=
-ARCH_ARMHF=
-ARCH_AARCH64=
+[ -z "$ARCH" ] && ARCH="unknown"
+
+# 默认使用GO语言的架构描述
+# REF: https://golang.org/doc/install/source#environment
+ARCH_386="386"
+ARCH_AMD64="amd64"
+ARCH_ARM="arm"
+ARCH_ARM64="arm64"
 
 exit_err() {
     [ -n "$@" ] && echo "$@" >&2
     exit 1
 }
 
-# 使用GO语言的架构描述
-# REF: https://golang.org/doc/install/source#environment
-arch_go_tpl() {
-    ARCH_X86="386"
-    ARCH_X86_64="amd64"
-    ARCH_ARMHF="arm"
-    ARCH_AARCH64="arm64"
-}
-
 arch_mark() {
+    [ -n "$USE_RAW" ] && {
+        # 输出环境原生描述
+        echo $ARCH
+        return 0
+    }
     local mark=$ARCH
-    [ -n "$USE_GO_ARCH_TPL" ] && arch_go_tpl
     case "$ARCH" in
-        "x86" )
-            [ -n "$ARCH_X86" ] && mark=$ARCH_X86
+        i386|x86 )
+            [ -n "$ARCH_386" ] && mark=$ARCH_386
             ;;
-        "x86_64" )
-            [ -n "$ARCH_X86_64" ] && mark=$ARCH_X86_64
+        amd64|x86_64 )
+            [ -n "$ARCH_AMD64" ] && mark=$ARCH_AMD64
             ;;
-        "armhf" )
-            [ -n "$ARCH_ARMHF" ] && mark=$ARCH_ARMHF
+        armhf )
+            [ -n "$ARCH_ARM" ] && mark=$ARCH_ARM
             ;;
-        "aarch64" )
-            [ -n "$ARCH_AARCH64" ] && mark=$ARCH_AARCH64
+        arm64|aarch64 )
+            [ -n "$ARCH_ARM64" ] && mark=$ARCH_ARM64
             ;;
     esac
     echo $mark
 }
 
 check() {
-    [ -z "$1" ] && exit_err "ERROR: arch mark missing."
-    [ "$(arch_mark)" == "$1" ] && exit 0 || exit 1
+    if [ "$CHECK_ARCH" = "check_" ]; then
+        exit_err "ERROR: arch mark missing."
+        exit 1
+    fi
+    [ "check_$(arch_mark)" = "$CHECK_ARCH" ]
+    exit $?
 }
 
 print() {
-    echo "$@$(arch_mark)"
+    echo "$1$(arch_mark)"
 }
 
 help() {
 cat <<EOF
-USAGE: $0 [COMMAND] [OPTION]
+USAGE: $0 [OPTION]
 
-Command:
-    print           输出arch，可通过参数自定义
-    check ARCH      检测arch
-    help            Help
+系统架构，默认使用GO语言的架构描述。
 
 Options:
-    --go                使用GO语言的架构描述
+    --check|-c MARK     比较检测arch
+    --raw|-r            使用环境原生的架构描述
                         有此参数时将忽略下列自定义输出
-    --x86 MARK          自定义x86架构输出
-    --x86_64 MARK       自定义x86_64架构输出
-    --armhf MARK        自定义arm/armhf架构输出
-    --aarch64 MARK      自定义aarch64/arm64架构输出
+    --386 MARK          自定义386/i386/x86架构输出
+    --amd64 MARK        自定义amd64/x86_64架构输出
+    --arm MARK          自定义arm/armhf架构输出
+    --arm64 MARK        自定义aarch64/arm64架构输出
+
+    --help|-h           帮助
+    --version|-v        版本
 EOF
 }
 
-COMMAND="print"
-if [ ${#} -gt 0 ]; then
-    cmd=$1
-    if [ "${cmd:0:1}" != "-" ]; then
-        COMMAND=$cmd
-        shift 1
-    fi
-fi
-
 while [ ${#} -gt 0 ]; do
     case "$1" in
-        --go )
-            USE_GO_ARCH_TPL=true
+        --version|-v )
+            echo "arch v$VERSION"
+            exit 0
             ;;
-        --x86 )
-            shift 1
-            ARCH_X86=$1
+        --help|-h )
+            help
+            exit 0
             ;;
-        --x86_64 )
+        --check|-c )
             shift 1
-            ARCH_X86_64=$1
+            CHECK_ARCH=check_$1
             ;;
-        --armhf )
-            shift 1
-            ARCH_ARMHF=$1
+        --raw|-r )
+            USE_RAW=true
             ;;
-        --aarch64 )
+        --386 )
             shift 1
-            ARCH_AARCH64=$1
+            ARCH_386=$1
+            ;;
+        --amd64 )
+            shift 1
+            ARCH_AMD64=$1
+            ;;
+        --arm )
+            shift 1
+            ARCH_ARM=$1
+            ;;
+        --amd64 )
+            shift 1
+            ARCH_AMD64=$1
             ;;
         -* )
             exit_err "ERROR OPTION: $1"
@@ -121,16 +138,8 @@ while [ ${#} -gt 0 ]; do
     shift 1
 done
 
-case "$COMMAND" in
-    "help" )
-        help
-        ;;
-    "print" )
-        print $@
-        ;;
-    "check" )
-        check "$1"
-        ;;
-    * )
-        exit_err "ERROR COMMAND: $COMMAND"
-esac
+if [ -n "$CHECK_ARCH" ]; then
+    check
+fi
+
+print "$@"
--- ubuntu/arch.sh
+++ ubuntu/arch.sh
@@ -1,47 +1,49 @@
 #!/usr/bin/env sh
 #
-# CPU架构信息，默认:
-# alpine环境等于`apk --print-arch`: x86 x86_64 armhf aarch64
-# debian/ubuntu/raspbian环境等于`dpkg --print-architecture`: i386 amd64 armhf arm64
+# CPU架构信息
+# 386   amd64   arm     arm64     默认: GO语言的架构描述
+#                                 原生:
+# x86   x86_64  armhf   aarch64     alpine: 等同于于`apk --print-arch`
+# i386  amd64   armhf   arm64       debian/ubuntu/raspbian: 等同于`dpkg --print-architecture`
 #
-# 也可使用GO语言的架构描述或自定义描述
+# 也可自定义描述
 #
 # AUTHOR:   JinnLynn <eatfishlin@gmail.com>
-# DATE:     2018-09-12
+# DATE:     2018-09-26
 #
-ARCH=x86_64
+
+VERSION=2018.09.26
+# 架构的系统原生描述
+ARCH=
+# 是否使用当前环境原生输出
+USE_RAW=
+# 要比较的字符串
+CHECK_ARCH=
 
 which dpkg 1>/dev/null 2>&1 && ARCH=$(dpkg --print-architecture)
 which apk 1>/dev/null 2>&1 && ARCH=$(apk --print-arch)
 
 [ -z "$ARCH" ] && ARCH="unknown"
 
-# 是否使用GO语言的架构描述
-USE_GO_ARCH_TPL=
-
-# 自定义架构描述
-ARCH_386=
-ARCH_AMD64=
-ARCH_ARM=
-ARCH_ARM64=
+# 默认使用GO语言的架构描述
+# REF: https://golang.org/doc/install/source#environment
+ARCH_386="386"
+ARCH_AMD64="amd64"
+ARCH_ARM="arm"
+ARCH_ARM64="arm64"
 
 exit_err() {
     [ -n "$@" ] && echo "$@" >&2
     exit 1
 }
 
-# 使用GO语言的架构描述
-# REF: https://golang.org/doc/install/source#environment
-arch_go_tpl() {
-    ARCH_386="386"
-    ARCH_AMD64="amd64"
-    ARCH_ARM="arm"
-    ARCH_ARM64="arm64"
-}
-
 arch_mark() {
+    [ -n "$USE_RAW" ] && {
+        # 输出环境原生描述
+        echo $ARCH
+        return 0
+    }
     local mark=$ARCH
-    [ -n "$USE_GO_ARCH_TPL" ] && arch_go_tpl
     case "$ARCH" in
         i386|x86 )
             [ -n "$ARCH_386" ] && mark=$ARCH_386
@@ -60,48 +62,54 @@ arch_mark() {
 }
 
 check() {
-    [ -z "$1" ] && exit_err "ERROR: arch mark missing."
-    [ "$(arch_mark)" == "$1" ] && exit 0 || exit 1
+    if [ "$CHECK_ARCH" = "check_" ]; then
+        exit_err "ERROR: arch mark missing."
+        exit 1
+    fi
+    [ "check_$(arch_mark)" = "$CHECK_ARCH" ]
+    exit $?
 }
 
 print() {
-    echo "$@$(arch_mark)"
+    echo "$1$(arch_mark)"
 }
 
 help() {
 cat <<EOF
-USAGE: $0 [COMMAND] [OPTION]
+USAGE: $0 [OPTION]
 
-Command:
-    print           输出arch，可通过参数自定义
-    check ARCH      检测arch
-    help            Help
+系统架构，默认使用GO语言的架构描述。
 
 Options:
-    --go                使用GO语言的架构描述
+    --check|-c MARK     比较检测arch
+    --raw|-r            使用环境原生的架构描述
                         有此参数时将忽略下列自定义输出
     --386 MARK          自定义386/i386/x86架构输出
     --amd64 MARK        自定义amd64/x86_64架构输出
     --arm MARK          自定义arm/armhf架构输出
     --arm64 MARK        自定义aarch64/arm64架构输出
+
+    --help|-h           帮助
+    --version|-v        版本
 EOF
 }
 
-COMMAND="print"
-if [ ${#} -gt 0 ]; then
-    cmd=$1
-    # debian/ubuntu/raspbian下sh实际为dash不支持${cmd:0:1}形式的切割字符串语法
-    # REF: https://wiki.ubuntu.com/DashAsBinSh
-    if [ "$(echo $cmd | awk '{ s=substr($0, 1, 1); print s; }')" != "-" ]; then
-        COMMAND=$cmd
-        shift 1
-    fi
-fi
-
 while [ ${#} -gt 0 ]; do
     case "$1" in
-        --go )
-            USE_GO_ARCH_TPL=true
+        --version|-v )
+            echo "arch v$VERSION"
+            exit 0
+            ;;
+        --help|-h )
+            help
+            exit 0
+            ;;
+        --check|-c )
+            shift 1
+            CHECK_ARCH=check_$1
+            ;;
+        --raw|-r )
+            USE_RAW=true
             ;;
         --386 )
             shift 1
@@ -130,16 +138,8 @@ while [ ${#} -gt 0 ]; do
     shift 1
 done
 
-case "$COMMAND" in
-    "help" )
-        help
-        ;;
-    "print" )
-        print $@
-        ;;
-    "check" )
-        check "$1"
-        ;;
-    * )
-        exit_err "ERROR COMMAND: $COMMAND"
-esac
+if [ -n "$CHECK_ARCH" ]; then
+    check
+fi
+
+print "$@"
