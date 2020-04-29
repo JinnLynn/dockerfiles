1ef8935f5b068442e74c651451d6f2463d4465c0
JinnLynn <eatfishlin@gmail.com>
2020-04-29 14:46:06 +0800

    基础镜像改为alpine, 分拆python2 python3

--- selenium/demo.py
+++ selenium/demo.py
@@ -1,6 +1,3 @@
-# -*- coding: utf-8 -*-
-from __future__ import (unicode_literals, absolute_import,
-                        division, print_function)
 import os
 import time
 
@@ -9,15 +6,12 @@ from selenium.webdriver import Chrome, ChromeOptions
 print('Docker & Chrome & Selenium Demo...')
 
 options = ChromeOptions()
-#! 必须带此参数 否则无法正常启动
 options.add_argument('--no-sandbox')
-# 无界面
 options.add_argument('--headless')
 options.add_argument('--disable-gpu')
-# 隐身模式
 options.add_argument('--incognito')
 
-brower = Chrome(chrome_options=options)
+brower = Chrome(options=options)
 brower.get('http://icanhazip.com')
 time.sleep(1)
 
