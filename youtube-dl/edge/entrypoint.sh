ba0a7de61c79ea844c93c47eca616268daaf4c0e
JinnLynn <eatfishlin@gmail.com>
2020-05-11 13:20:47 +0800

    优化edge更新方式; 优化命令调用方法

--- youtube-dl/edge/entrypoint.sh
+++ youtube-dl/edge/entrypoint.sh
@@ -1,12 +1,38 @@
 #!/usr/bin/env sh
 
-cd ${REPO_DIR}
+LAST_UPDATE=${LAST_UPDATE:-/app/tmp/last-update}
+PULL_INTERVAL=${PULL_INTERVAL:-86400}
 
-# 更新
-git pull --depth 1 -f --recurse-submodules -q -r 2>/dev/null
+update() {
+    cd ${REPO_LOCAL}
+    ts=$(date +%s)
+    if [ -e .git ]; then
+        last_update=$(cat $LAST_UPDATE 2>/dev/null)
+        [ -z "$last_update" ] && last_update=0
+        diff=$(echo "$ts-$last_update" | bc)
+        if [ "$diff" -gt $PULL_INTERVAL ]; then
+            echo "Pull..."
+            git pull --recurse-submodules --depth 1 -qfr
+            echo $ts >$LAST_UPDATE
+        fi
+    else
+        echo "Clone..."
+        git clone --recurse-submodules --depth 1 --single-branch -q ${REPO_REMOTE} .
+        pip install -qe .
+        echo $ts >$LAST_UPDATE
+    fi
+    cd - >/dev/null
+}
 
-cd - >/dev/null
+update
 
-exec youtube-dl --external-downloader aria2c \
-                --external-downloader-args "--file-allocation=none --summary-interval=0 --max-connection-per-server=16" \
-                $@
+# 直接指定参数 调用外部下载工具多线程下载
+if [ "${1:0:1}" = '-' ]; then
+    set -- youtube-dl --external-downloader aria2c \
+                      --external-downloader-args --file-allocation=none \
+                      --external-downloader-args --summary-interval=60 \
+                      --external-downloader-args --max-connection-per-server=16 \
+                      $@
+fi
+
+exec $@
