d11554d4f445f6d0a58bb78c20dfcdde3b2304c0
JinnLynn <eatfishlin@gmail.com>
2022-04-13 10:42:53 +0800

    ipv6

--- ddns/entrypoint.sh
+++ ddns/entrypoint.sh
@@ -2,40 +2,55 @@
 # dnspod token
 DDNS_TOKEN=${DDNS_TOKEN:-}
 DDNS_DOMAIN=${DDNS_DOMAIN:-}
-DDNS_IP_SERVICE=${DDNS_IP_SERVICE:-http://ip.3322.net}
-DDNS_CHECK_INTERVAL=${DDNS_CHECK_INTERVAL:-60}
-DDNS_IPV6=${DDNS_IPV6:-}
 
-DDNS_IP=${DDNS_IP:-}
-DDNS_PSL=${DDNS_PSL:-/app/etc/psl.dat}
+DDNS_TYPE_IPV4=${DDNS_TYPE_IPV4:-1}
+DDNS_IPV4=${DDNS_IPV4:-}
+DDNS_IPV4_SERVICE=${DDNS_IP_SERVICE:-http://ip.3322.net}
+
+DDNS_TYPE_IPV6=${DDNS_TYPE_IPV6:-0}
+DDNS_IPV6=${DDNS_IPV6:-}
+DDNS_IPV6_SERVICE=${DDNS_IPV6_SERVICE:-https://ipv6.icanhazip.com}
+
+DDNS_CHECK_INTERVAL=${DDNS_CHECK_INTERVAL:-60}
+DDNS_HOOK=${DDNS_HOOK:-}
+DDNS_PSL=${DDNS_PSL:-/app/var/psl.dat}
 
 CONNECT_TIMEOUT=5
 SILENT=
 REPEAT=
 
+DEF_CURL_OPTS="-sL -X POST --connect-timeout ${CONNECT_TIMEOUT:-5}"
+
 PSL="psl --load-psl-file $DDNS_PSL --print-reg-domain -b"
 
-is_ip() {
-    [[ "$1" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]] && return 0 || return 1
+is_ipv4() {
+    # REF: https://helloacm.com/how-to-valid-ipv6-addresses-using-bash-and-regex/
+    [[ "$1" =~ ^([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])$ ]]
 }
 
-get_ip() {
-    local ip=$1
-    [ -n "$ip" ] && {
-        is_ip $ip || {
-            log_err "Invalid IP address: $ip"
-            return 1
-        }
-    } || {
-        local opts=
-        [ -n "$DDNS_IPV6" ] && opts="--ipv6" || opts="--ipv4"
-        ip=$(curl -s $opts $DDNS_IP_SERVICE)
-        is_ip $ip || {
-            log_err "fetch current ip fail."
-            return 1
-        }
+is_ipv6() {
+    # REF: https://helloacm.com/how-to-valid-ipv6-addresses-using-bash-and-regex/
+    [[ "$1" =~ ^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$ ]]
+}
+
+get_ipv4() {
+    [[ -f "$DDNS_HOOK" ]] && source "$DDNS_HOOK"
+    # local ip=$DDNS_IPV4
+    # is_ipv4 "$ip" || ip=$(curl -sL4 $DDNS_IPV4_SERVICE 2>/dev/null)
+    local ip=${DDNS_IPV4:-$(curl -sL4 $DDNS_IPV4_SERVICE 2>/dev/null)}
+    type -a on_get_ipv4 1>/dev/null 2>&1 && {
+        ip=$(on_get_ipv4 "$ip")
     }
-    echo $ip
+    is_ipv4 "$ip" && echo $ip
+}
+
+get_ipv6() {
+    [[ -f "$DDNS_HOOK" ]] && source "$DDNS_HOOK"
+    local ip=${DDNS_IPV6:-$(curl -sL6 $DDNS_IPV6_SERVICE 2>/dev/null)}
+    type -a on_get_ipv6 1>/dev/null 2>&1 && {
+        ip=$(on_get_ipv6 "$ip")
+    }
+    is_ipv6 "$ip" && echo $ip
 }
 
 log() {
@@ -43,50 +58,79 @@ log() {
 }
 
 log_err() {
-    log $@ 1>&2
+    echo $(date +%Y-%m-%d\ %H:%M:%S) $@ 1>&2
 }
 
-jq_parse() {
-    echo "$1" | jq -r "$2" 2>/dev/null
+# post_api ACTION [PARAMS]
+post_api() {
+    local params="login_token=$DDNS_TOKEN&format=json&domain=$domain&sub_domain=$sub_domain"
+    [[ -n "$2" ]] && params="${params}&$2"
+    curl $DEF_CURL_OPTS -o $tmp https://dnsapi.cn/$1 -d "$params"
 }
 
-try_update() {
-    domain=$1
-    sub_domain=$2
+update() {
+    local record_type=$(echo ${1:-A} | tr [:lower:] [:upper:])
+    local cur_ip=
 
-    common_params="login_token=$DDNS_TOKEN&format=json&domain=$domain&sub_domain=$sub_domain"
+    [[ "$record_type" == "AAAA" ]] && cur_ip=$(get_ipv6) || cur_ip=$(get_ipv4)
 
-    curl_opt="-s -X POST --connect-timeout ${CONNECT_TIMEOUT:-5}"
-    # ddns ip
-    ip=$(get_ip $DDNS_IP)
-    [ -z "$ip" ] && return 1
-
-    # 获取sub_domain record id
-    res=$(curl $curl_opt https://dnsapi.cn/Record.List -d "$common_params")
-    code=$(jq_parse "$res" ".status.code | tonumber")
-    [ "$code" != 1 ] && {
-        log_err "Fetch host info fail:" $(jq_parse "$res" .status.message)
+    [[ -z "$cur_ip" ]] && {
+        log_err "Fetch Current IP[$record_type] fail."
         return 1
     }
 
-    record_ip=$(jq_parse "$res" ".records[0].value")
-    record_id=$(jq_parse "$res" ".records[0].id | tonumber")
+    # echo $cur_ip
+
+    local tmp=$(mktemp)
+
+    post_api Record.List "record_type=$record_type" >$tmp
+    # cat $tmp | jq
+
+    local record_id=$(cat $tmp | jq -r '.records[0].id')
+    local record_value=$(cat $tmp | jq -r '.records[0].value')
+    [[ "$record_id" == "null" ]] && record_id=""
+    [[ "$record_value" == "null" ]] && record_value=""
+
+    [[ -z "$record_id" ]] && {
+        cat $tmp | jq -e '.status.code == "10"' >/dev/null && {
+            # 记录不存在
+            log "Create $record_type record."
+            post_api Record.Create "record_type=$record_type&record_line=默认&value=$cur_ip" >$tmp
+            return
+        }
+    }
+
+    [[ -z "$record_id" ]] && {
+        log_err "Get $record_type record fail."
+        return 1
+    }
 
     # 记录已经与当前IP相同
-    [ "$ip" == "$record_ip" ] && {
-        [ -z "$SILENT" ] && log "DDNS domain $DDNS_DOMAIN[$ip] unchanged."
+    [ "$cur_ip" == "$record_value" ] && {
+        [ -z "$SILENT" ] && log "DDNS $DDNS_DOMAIN[$record_type $cur_ip] unchanged."
         return
     }
 
-    # 更新
-    res=$(curl $curl_opt "https://dnsapi.cn/Record.Ddns" -d "$common_params&record_id=$record_id&value=$ip&record_line=默认")
-    code=$(jq_parse "$res" ".status.code | tonumber")
-    [ "$code" != 1 ] && {
-        log_err "Update DDNS Hostname $DDNS_DOMAIN fail:" $(jq_parse "$res" .status.message)
+
+    [[ "$record_type" == "A" ]] && {
+        post_api Record.Ddns "record_id=$record_id&value=$cur_ip&record_line=默认" >$tmp
+    } || {
+        post_api Record.Modify "record_id=$record_id&value=$cur_ip&record_line=默认&record_type=AAAA" >$tmp
+    }
+
+    # cat $tmp | jq
+
+    cat $tmp | jq -e '.status.code == "1"' >/dev/null || {
+        log_err "DDNS $DDNS_DOMAIN[$record_type] update fail:" $(cat $tmp | jq .status.message)
         return 1
     }
 
-    log "DDNS Hostname $DDNS_DOMAIN[$ip] updated."
+    log "DDNS $DDNS_DOMAIN[$record_type $cur_ip] updated."
+}
+
+try_update() {
+    [[ "$DDNS_TYPE_IPV4" == 1 ]] && update a
+    [[ "$DDNS_TYPE_IPV6" == 1 ]] && update aaaa
 }
 
 trap_update() {
@@ -98,21 +142,33 @@ while (( ${#} )); do
     case "$1" in
         --token|-t )
             shift 1
-            DDNS_TOKEN=${1:-}
+            DDNS_TOKEN=$1
             ;;
         --domain|-d )
             shift 1
-            DDNS_DOMAIN=${1:-}
+            DDNS_DOMAIN=$1
             ;;
-        --ip|-i )
+        --ip|--ipv4 )
             shift 1
-            DDNS_IP=${1:-}
+            DDNS_IPV4=$1
+            is_ipv4 "$DDNS_IPV4" || {
+                log_err "ipv4[$DDNS_IPV4] error."
+                exit 1
+            }
+            ;;
+        --ipv6 )
+            shift 1
+            DDNS_IPV6=$1
+            is_ipv4 "$DDNS_IPV6" || {
+                log_err "ipv6[$DDNS_IPV6] error."
+                exit 1
+            }
             ;;
         -4 )
-            DDNS_IPV6=
+            DDNS_TYPE_IPV4=1
             ;;
         -6 )
-            DDNS_IPV6=true
+            DDNS_TYPE_IPV6=1
             ;;
         --slient|-s )
             SILENT=true
@@ -120,12 +176,16 @@ while (( ${#} )); do
         --repeat|-r )
             REPEAT=true
             ;;
+        --hook )
+            shift 1
+            DDNS_HOOK=$1
+            ;;
         --help|-h )
             echo "USAGE: $0 [-t|--token TOKEN] [-d|--domain DOMAIN] [--ip|-i IP] [--repeat|-r] [--slient|-s] [-6|-4]"
             exit 0
             ;;
         -* )
-            log_err "错误的选项: $1"
+            log_err "Argument ERROR: $1"
             ;;
         * )
             # 参数结束
@@ -141,14 +201,19 @@ done
 domain=$($PSL $DDNS_DOMAIN)
 sub_domain=${DDNS_DOMAIN%.$domain}
 
+[[ "$DDNS_TYPE_IPV4" != 1 ]] && [[ "$DDNS_TYPE_IPV6" != 1 ]] && DDNS_TYPE_IPV4=1
+
 echo "====="
-echo "DOMAIN:   $DDNS_DOMAIN($sub_domain $domain)"
-echo "TOKEN:    $DDNS_TOKEN"
-echo "IP_SRV:   $DDNS_IP_SERVICE"
+echo "Domain:   $DDNS_DOMAIN($sub_domain $domain)"
+echo "Token:    $DDNS_TOKEN"
 [[ -n "$REPEAT" ]] && \
     echo "INTERVAL: $DDNS_CHECK_INTERVAL"
-[ -n "$DDNS_IP" ] && \
-    echo "IP:       $DDNS_IP"
+[[ "$DDNS_TYPE_IPV4" == 1 ]] && {
+    echo "IPv4:     ${DDNS_IPV4:-$DDNS_IPV4_SERVICE}"
+}
+[[ "$DDNS_TYPE_IPV6" == 1 ]] && {
+    echo "IPv6:     ${DDNS_IPV6:-$DDNS_IPV6_SERVICE}"
+}
 echo "====="
 echo
 
---
+++ ddns/hook.example.sh
@@ -0,0 +1,10 @@
+# CALL BY: on_get_ipv4 CURRENT_IPV4
+on_get_ipv4() {
+    echo "$1"
+}
+
+
+# CALL BY: on_get_ipv6 CURRENT_IPV6
+on_get_ipv6() {
+    echo "$1"
+}
