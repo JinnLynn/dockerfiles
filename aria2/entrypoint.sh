5739c78274008c27cd10cbde925aec4cf7188812
JinnLynn <eatfishlin@gmail.com>
2022-08-30 15:50:32 +0800

    update

--- aria2/entrypoint.sh
+++ aria2/entrypoint.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
 
-ARIA2_CONFIG=${AEIA2_CONFIG:-/app/etc/aria2.conf}
+: ${AEIA2_CONFIG:="/app/etc/aria2.conf"}
 
 if [ -z "$@" ]; then
     set -- aria2c ${ARIA2_CONFIG:+"--conf-path=$ARIA2_CONFIG"} \
                 ${ARIA2_RPC_SECRET:+"--rpc-secret=$ARIA2_RPC_SECRET --enable-rpc=true"}
 fi
 
-if [ "${1:0:1}" = '-' ]; then
+if [ "${1:0:1}" = "-" ]; then
     set -- aria2c $@
 fi
 
